c834bd26f276ff7e8b54e172b764f9684de67c9b
troooonks <troooonks@gmail.com>
2022-06-25 00:49:51 +0200

    funcionando 1 turno

--- piedra.js
+++ piedra.js
@@ -1,10 +1,9 @@
 
-let counterPlayer;
-let counterComputer;
+let counterPlayer = 0;
+let counterComputer = 0;
 
 function getComputerSelect() {
     let eleccionComputer;
-    let resultadoComputer;
 
     let computerPlay = Math.floor(Math.random() * 3 + 1);
 
@@ -17,7 +16,7 @@ function getComputerSelect() {
     } else {
         document.write("La Computadora está rota");
     }
-    document.write("<h1>el if de la maquina tiene el valor:"+eleccionComputer+"</h1>");
+    console.log("el if de la maquina tiene el valor:"+eleccionComputer);
     return eleccionComputer;
         
 }
@@ -33,35 +32,49 @@ function getPlayerSelect() {
 
 
 function comprobarGanador(eleccionComputer, eleccionMinusPlayer) {
-    
+
+
     if ((eleccionComputer == "piedra" && eleccionMinusPlayer == "tijera")|| 
     (eleccionComputer == "papel" && eleccionMinusPlayer == "piedra") ||
     (eleccionComputer == "tijera" && eleccionMinusPlayer == "papel")) {
         document.write("<h1> Perdiste </h1>");
-        counterComputer++;
-    } else if (eleccionComputer == eleccionMinusPlayer) {
+         } else if (eleccionComput<er == eleccionMinusPlayer) {
         document.write("<h1>Empate!</h1>");
     } else if ((eleccionMinusPlayer == "piedra" && eleccionComputer == "tijera")|| 
     (eleccionMinusPlayer == "papel" && eleccionComputer == "piedra") ||
     (eleccionMinusPlayer == "tijera" && eleccionComputer == "papel")) {
         document.write("<h1>Ganaste</h1>");
-        counterPlayer++;
+        counterPlayer = counterPlayer + 1;       
     } else {
-        document.write("<h1>Algo ha salido mal y no es tu culpa</h1>")
+        document.write("<h1>Algo ha salido mal y no es tu culpa</h1>");
     }
+
 }
 
-
-let playerHand = getPlayerSelect();
-let computerHand = getComputerSelect();
-
-
-
-
-
+function setScore() {
+if ((computerHand == "piedra" && playerHand == "tijera")|| 
+(computerHand == "papel" && playerHand == "piedra") ||
+(computerHand == "tijera" && playerHand == "papel")) {
+    counterComputer =  counterComputer + 1;
+} else if (computerHand == playerHand) {
+} else if ((playerHand == "piedra" && computerHand == "tijera")|| 
+(playerHand == "papel" && computerHand == "piedra") ||
+(playerHand == "tijera" && computerHand == "papel")) {
+    counterPlayer = counterPlayer + 1;       
+    } else {
+        document.write("<h1>Algo ha salido mal y no es tu culpa</h1>");
+    }
+    
+}
+    let playerHand = getPlayerSelect();
+    let computerHand = getComputerSelect();
 
 
 
 document.write("El jugador ha elegido: "+ playerHand +"<br> La maquina ha elegido: "+computerHand);
 
+for (let i = 0; i < 5; i++){
 comprobarGanador(computerHand, playerHand);
+setScore();
+document.write("<h3>Marcador:</h3><h2>Jugador: "+counterPlayer+"</h2><h2>Ordenador: "+counterComputer+"</h2>");
+}
