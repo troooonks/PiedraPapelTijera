df63c46e9fa1a5190fbae1aeca9f6f6bf33f440a
troooonks <troooonks@gmail.com>
2022-06-24 03:28:16 +0200

    corregido el error de comparar la palabra tijeras cuando he pasado la cadena tijera en singular

--- piedra.js
+++ piedra.js
@@ -12,10 +12,12 @@ function getComputerSelect() {
         eleccionComputer = "piedra";
     } else if (computerPlay == 2) {
         eleccionComputer = "papel";
-    } else {
+    } else if (computerPlay == 3) {
         eleccionComputer = "tijera";
+    } else {
+        document.write("La Computadora está rota");
     }
-
+    document.write("<h1>el if de la maquina tiene el valor:"+eleccionComputer+"</h1>");
     return eleccionComputer;
         
 }
@@ -24,35 +26,41 @@ function getComputerSelect() {
 function getPlayerSelect() {
         let eleccionPlayer = prompt("Elige piedra papel o tijera",);
         eleccionMinusPlayer = eleccionPlayer.toLowerCase();
+        document.write("<h1>"+eleccionMinusPlayer+"<h1>");
         return eleccionMinusPlayer;
+        
 }
 
+
 function comprobarGanador(eleccionComputer, eleccionMinusPlayer) {
-   
     
-    if (eleccionComputer == "piedra" && eleccionMinusPlayer == "tijeras" || 
-        eleccionComputer == "papel" && eleccionMinusPlayer == "piedra" ||
-        eleccionComputer == "tijeras" && eleccionMinusPlayer == "papel")  {
+    if ((eleccionComputer == "piedra" && eleccionMinusPlayer == "tijera")|| 
+    (eleccionComputer == "papel" && eleccionMinusPlayer == "piedra") ||
+    (eleccionComputer == "tijera" && eleccionMinusPlayer == "papel")) {
         document.write("<h1> Perdiste </h1>");
         counterComputer++;
     } else if (eleccionComputer == eleccionMinusPlayer) {
         document.write("<h1>Empate!</h1>");
-    } else {
+    } else if ((eleccionMinusPlayer == "piedra" && eleccionComputer == "tijera")|| 
+    (eleccionMinusPlayer == "papel" && eleccionComputer == "piedra") ||
+    (eleccionMinusPlayer == "tijera" && eleccionComputer == "papel")) {
         document.write("<h1>Ganaste</h1>");
         counterPlayer++;
+    } else {
+        document.write("<h1>Algo ha salido mal y no es tu culpa</h1>")
     }
 }
 
 
-
-
-
-
-
-
-
 let playerHand = getPlayerSelect();
 let computerHand = getComputerSelect();
+
+
+
+
+
+
+
 
 document.write("El jugador ha elegido: "+ playerHand +"<br> La maquina ha elegido: "+computerHand);
 
